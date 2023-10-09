f84a3f998616a94d775d3b19889cd3f7f79e6c25
Kristofferobling <115920215+Kristofferobling@users.noreply.github.com>
2023-10-09 14:43:10 +0200

    Update

--- app.js
+++ app.js
@@ -43,19 +43,65 @@ function ClickOnCookie() {
 }
 
 function DoubleCookies() {
+    //Level 2
     if (cookieMultiplier === multiplierCookies.level1) {
         if (cookies >= multiplierCookiePrice.level2Price) {
             cookies -= multiplierCookiePrice.level2Price;
             document.getElementById("cookieknap").innerHTML = `4 x Cookies`;
             cookieMultiplier = multiplierCookies.level2;
         }
+        //Level 3
     } else if (cookieMultiplier === multiplierCookies.level2) {
         if (cookies >= multiplierCookiePrice.level3Price) {
             cookies -= multiplierCookiePrice.level3Price;
             document.getElementById("cookieknap").innerHTML = `8 x Cookies`;
             cookieMultiplier = multiplierCookies.level3;
         }
+        // Level 4
+    }  else if (cookieMultiplier === multiplierCookies.level3) {
+        if (cookies >= multiplierCookiePrice.level4Price) {
+            cookies -= multiplierCookiePrice.level4Price;
+            document.getElementById("cookieknap").innerHTML = `16 x Cookies`;
+            cookieMultiplier = multiplierCookies.level4;
+        }
+        //Level 5
+    } else if (cookieMultiplier === multiplierCookies.level4) {
+        if (cookies >= multiplierCookiePrice.level5Price) {
+            cookies -= multiplierCookiePrice.level5Price;
+            document.getElementById("cookieknap").innerHTML = `32 x Cookies`;
+            cookieMultiplier = multiplierCookies.level5;
+        }
+        //Level 6
+    } else if (cookieMultiplier === multiplierCookies.level5) {
+        if (cookies >= multiplierCookiePrice.level6Price) {
+            cookies -= multiplierCookiePrice.level6Price;
+            document.getElementById("cookieknap").innerHTML = `64 x Cookies`;
+            cookieMultiplier = multiplierCookies.level6;
+        }
+        //Level 7
+    } else if (cookieMultiplier === multiplierCookies.level6) {
+        if (cookies >= multiplierCookiePrice.level7Price) {
+            cookies -= multiplierCookiePrice.level7Price;
+            document.getElementById("cookieknap").innerHTML = `128 x Cookies`;
+            cookieMultiplier = multiplierCookies.level7;
+        }
+        //Level 8
+    } else if (cookieMultiplier === multiplierCookies.level7) {
+        if (cookies >= multiplierCookiePrice.level8Price) {
+            cookies -= multiplierCookiePrice.level8Price;
+            document.getElementById("cookieknap").innerHTML = `256 x Cookies`;
+            cookieMultiplier = multiplierCookies.level8;
+        }
+        //Level 9
+    } else if (cookieMultiplier === multiplierCookies.level8) {
+        if (cookies >= multiplierCookiePrice.level9Price) {
+            cookies -= multiplierCookiePrice.level9Price;
+            document.getElementById("cookieknap").innerHTML = `512 x Cookies`;
+            cookieMultiplier = multiplierCookies.level9;
+        }
     }
-}
+ }
+
+
     UpdateCookieCounter();
 
